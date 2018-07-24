5c680452f1e279c3deaeac098f8450acb27a1437
DK\whc <whc@sitecore.net>
2018-07-24 14:47:34 +0800

    13605. Add if() checking that exist in original SiteResolver but missing in SXA SiteResolver.

---
+++ src/Sitecore.Support.13605/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.13605")]
+[assembly: AssemblyProduct("Sitecore.Support.13605")]
+[assembly: ComVisible(false)]
